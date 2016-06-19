d49121c1e653bbe54be1f445da994d4bb7385285
HarrisLeow <leow_harris@hotmail.sg>
2016-06-19 22:22:15 +0800

    creaed new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about'
 
+  resources :articles
+  #adds all the HTTP Actiions
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
