50be80fae69759d82917267f78abcc1010f7422d
HarrisLeow <leow_harris@hotmail.sg>
2016-06-20 22:33:08 +0800

    refactored controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   def index
     @articles = Article.all
   end
@@ -25,23 +25,20 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
   end
 
   def edit
-    @article = Article.find(params[:id])
 
   end
 
   def update
-    @article = Article.find(params[:id])
+
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -54,11 +51,11 @@ class ArticlesController < ApplicationController
 
 
 
-
-
-
-
   private
+  def set_article
+    @article = Article.find(params[:id])
+
+  end
   def article_params
     params.require(:article).permit(:title, :description)
     #title: "This is a test title", description: "This is a test description"
